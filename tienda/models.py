855bd389194b1edb4fa140cf1554d61b577cfcef
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2022-11-11 13:13:55 +0100

    Modelos creados

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,23 @@
+import datetime
+
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=20, primary_key=True)
+class Producto(models.Model):
+    marca_producto= models.ForeignKey(Marca, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=20, primary_key=True)
+    modelo = models.CharField(max_length=20)
+    unidades = models.IntegerField()
+    precio = models.FloatField()
+    detalles = models.CharField(max_length=20)
+class Compra(models.Model):
+    nombre_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha =models.DateTimeField(default=timezone.now)
+    unidades =models.IntegerField()
+    importe = models.FloatField()
+
+
