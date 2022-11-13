d4a7a5dc358d4557c63a04424d11fc870574735c
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2022-11-13 13:12:00 +0100

    Lista hecha

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Marca,Producto
+from .models import Marca,Producto,Compra
 
 admin.site.register(Marca)
 admin.site.register(Producto)
+admin.site.register(Compra)
 # Register your models here.
